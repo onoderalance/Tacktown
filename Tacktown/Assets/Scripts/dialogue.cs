057b281cd6c919553f99c61274d5c1f5a2289402
aria nguyen <arianakamura@gmail.com>
2025-01-25 01:12:37 -0500

    Update dialogue.cs

--- Tacktown/Assets/Scripts/dialogue.cs
+++ Tacktown/Assets/Scripts/dialogue.cs
@@ -20,7 +20,7 @@ public class dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.getMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             if (textComponent.text == lines[index])
             {
