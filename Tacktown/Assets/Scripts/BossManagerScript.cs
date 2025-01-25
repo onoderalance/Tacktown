466524a2d7aec006269809f57d481c43444e9482
hvandermillen <hvandermillen@gmail.com>
2025-01-25 16:49:26 -0500

    im still working on that boss fight manager script

--- Tacktown/Assets/Scripts/BossManagerScript.cs
+++ Tacktown/Assets/Scripts/BossManagerScript.cs
@@ -8,6 +8,17 @@ public class BossManagerScript : MonoBehaviour
     public GameObject homingMissile;
     public GameObject rotatingSpike;
 
+    float preciseTimer = 0.0f; //delta time
+    int stepCounter = 0; //increases every (stepTime) seconds
+    bool stepUpdated = false;
+    public float stepTime = 1.0f;
+
+    enum Attacks    {SHOT_FROM_TOP, SHOT_FROM_BOTTOM, SHOT_FROM_LEFT, SHOT_FROM_RIGHT,
+                    BURST_FROM_TOP, BURST_FROM_BOTTOM, BURST_FROM_LEFT, BURST_FROM_RIGHT,
+                    SHOT_FROM_CENTER, MISSILE_FROM_CENTER};
+
+    public Dictionary<int, > attackList; //maps a time to an attack
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +31,21 @@ public class BossManagerScript : MonoBehaviour
         
     }
 
+    //only called when the step counter is updated
+    void onStepUpdate() {
+
+        
+
+        stepUpdated = false;
+    }
+
+
+
     //fires a single shot at the given x position from the top of the screen downwards
     void singleShotFromTop(float xPos)
     {
         float yPos = -0.5f;
-        Vector3 spawnPosition = new Vector3(xPos, -0.5, 0)
+        Vector3 spawnPosition = new Vector3(xPos, yPos, 0)
         GameObject newProjectile = Instantiate(projectile, spawnPosition, new Vector3(0,0,0));
 
     }
@@ -32,11 +53,52 @@ public class BossManagerScript : MonoBehaviour
     //fires several shots down from the top of the screen, starting at xPos
     //if timeOffset is a positive value, these are shot one after another,
 	//with timeOffset representing the time between each shot
-    void burstFromTop(float xPos, float timeOffset) { }
+    void burstFromTop(float xPos, int numShots, float spaceOffset, float timeOffset) {
 
+    }
+
+    //fires a single shot at the given x position from the bottom of the screen upwards
     void singleShotFromBottom(float xPos)
     {
-        
+        float yPos = -1.78f;
+        Vector3 spawnPosition = new Vector3(xPos, yPos, 0)
+        GameObject newProjectile = Instantiate(projectile, spawnPosition, new Vector3(0, 0, 180));
+    }
+
+    void burstFromBottom(float xPos, int numShots, float spaceOffset, float timeOffset)
+    {
+
+    }
+
+    //fires a single shot at the given y position from the left of the screen rightwards
+    void singleShotFromLeft(float yPos)
+    {
+        float yPos = -0.5f;
+        Vector3 spawnPosition = new Vector3(xPos, -0.5, 0)
+        GameObject newProjectile = Instantiate(projectile, spawnPosition, new Vector3(0, 0, 0));
+    }
+
+
+    void burstFromLeft(float yPos, int numShots, float spaceOffset, float timeOffset)
+    {
+
+    }
+
+    void burstFromRight(float yPos, int numShots, float spaceOffset, float timeOffset)
+    {
+
+    }
+
+    //SHOTS FROM MIDDLE
+    Vector3 centerPosition = new Vector3(4,-6,0);
+
+    void singleShotFromCenter(float angle)
+    {
+        GameObject newProjectile = Instantiate(projectile, centerPosition, new Vector3(0, 0, angle));
+    }
+
+    void homingMissleFromCenter() {
+        GameObject newProjectile = Instantiate(homingMissile, centerPosition, new Vector3(0, 0, 0));
     }
 
 }
