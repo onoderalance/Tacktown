454a9716d240864403a764b0cceab73660668dcd
onoderalance <onoderalance@gmail.com>
2025-01-24 23:57:23 -0500

    added sprites

---
+++ Tacktown/Assets/Scripts/cutsceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutsceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called when running a given cutscene
+    // 
+    void RunScene()
+    {
+
+    }
+}
