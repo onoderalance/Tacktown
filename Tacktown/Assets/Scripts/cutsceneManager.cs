3884614faf70dac222af639635cb85511117a123
onoderalance <onoderalance@gmail.com>
2025-01-25 00:19:06 -0500

    made base functions

--- Tacktown/Assets/Scripts/cutsceneManager.cs
+++ Tacktown/Assets/Scripts/cutsceneManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class cutsceneManager : MonoBehaviour
 {
+    bool hudState = true;
+    public GameObject hudObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +16,36 @@ public class cutsceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            toggleHUD();
+        }
+        hudObject.SetActive(hudState);
     }
 
     // Called when running a given cutscene
-    // 
-    void RunScene()
+    // 1 - intro 2 - betrayal 3 - bad end 4 - good end
+    void runScene(int scene)
     {
 
     }
+
+    // Switch visual frame shown for the cutscenes
+    void switchFrame(int frame)
+    {
+
+    }
+
+    // Toggle HUD bar visual
+    void toggleHUD()
+    {
+        hudState = !hudState; 
+    }
+
+    // Set HUD bar visual
+    void setHUD(bool hud)
+    {
+        hudState = hud;
+    }
+
 }
