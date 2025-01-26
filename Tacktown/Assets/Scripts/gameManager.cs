0161424e29728836993e2afb3a1f5054c295f31a
aria nguyen <arianakamura@gmail.com>
2025-01-25 23:09:31 -0500

    game over stuff

---
+++ Tacktown/Assets/Scripts/gameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
